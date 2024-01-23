ff9f87a1d9cf6f7a3438d2bb79ceb86f43e685a9
dlysenko359 <dlysenko359@gmail.com>
2024-01-23 21:15:00 +0200

    write stream commit

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,7 +1,7 @@
 import {createWriteStream} from 'fs'
 import { fileURLToPath } from 'url';
 import path from 'path';
-import { Writable } from 'stream';
+import {Writable } from 'stream';
 
 const write = async () => {
     const __filename = fileURLToPath(import.meta.url);
@@ -9,8 +9,34 @@ const write = async () => {
     const fileToWritePath = path.join(__dirname, 'files', 'fileToWrite.txt');
 
     const writeStream = createWriteStream(fileToWritePath);
-    writeStream.write('Test text');
-    writeStream.end();
+
+    writeStream.on('finish', (data) => {
+        console.log('stream finished')
+    })
+    writeStream.on('error', (err) => {
+        console.log('stream throwed error' + err)
+    })
+    writeStream.on('close', () => {
+        console.log('stream closed')
+    })
+    writeStream.on('pipe', (data) => {
+        console.log('stream piped')
+    })
+    writeStream.on('unpipe', (data) => {
+        console.log('stream unpiped')
+    })
+
+    const stdinToWriteStream = new Writable({
+        write(chunk, encoding, callback) {
+            writeStream.write(chunk);
+            writeStream.end();
+            process.stdin.unpipe(stdinToWriteStream);
+            callback();
+        }
+    });
+
+    process.stdin.pipe(stdinToWriteStream)
+      
 };
 
 await write();
